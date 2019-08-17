b0f2bdc4e08275e290cd1c13384f448a455b6138
Keegan Crankshaw <kcranky@gmail.com>
2019-08-17 15:42:26 +0200

    Prac 4 ready for students

---
+++ Prac4/src/Prac4.cpp
@@ -0,0 +1,118 @@
+/*
+ * Prac4.cpp
+ * 
+ * Originall written by Stefan Schröder and Dillion Heald
+ * 
+ * Adapted for EEE3096S 2019 by Keegan Crankshaw
+ * 
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "Prac4.h"
+
+using namespace std;
+
+int chan = 0;
+bool playing = true; // should be set false when paused
+bool stopped = false; // If set to true, program should close
+unsigned char buffer[2][BUFFER_SIZE][2];
+int buffer_location = 0;
+bool bufferReading = 0; //using this to switch between column 0 and 1 - the first column
+bool threadReady = false; //using this to finish writing the first column at the start of the song, before the column is played
+
+
+// Configure your interrupts here.
+// Don't forget to use debouncing.
+void play_pause_isr(void){
+    //Write your logis here
+}
+
+void stop_isr(void){
+    // Write your logic here
+}
+
+/*
+ * Setup Function. Called once 
+ */
+int setup_gpio(void){
+    //Set up wiring Pi
+    wiringPiSetup();
+    //setting up the buttons
+
+    //setting upt the SPI interface
+    
+    return 0;
+}
+
+/* 
+ * Thread that handles writing to SPI
+ * 
+ * You must pause writing to SPI if not playing is true (the player is paused)
+ * When calling the function to write to SPI, take note of the last argument.
+ * You don't need to use the returned value from the wiring pi SPI function
+ * You need to use the buffer_location variable to check when you need to switch buffers
+ */
+void *playThread(void *threadargs){
+    // If the thread isn't ready, don't do anything
+    while(!threadReady)
+        continue;
+    
+    //You need to only be playing if the stopped flag is false
+    while(!stopped){
+        //Code to suspend playing if paused
+        
+        //Write the buffer out to SPI
+        
+        //Do some maths to check if you need to toggle buffers
+        
+    }
+    
+    pthread_exit(NULL);
+}
+
+int main(){
+    // Call the setup GPIO function
+	if(setup_gpio()==-1){
+        return 0;
+    }
+    
+    /* Initialize thread with parameters
+     * Set the play thread to have a 99 priority
+     * Read https://docs.oracle.com/cd/E19455-01/806-5257/attrib-16/index.html
+     */ 
+    
+    //Write your logic here
+    
+    /*
+     * Read from the file, character by character
+     * You need to perform two operations for each character read from the file
+     * You will require bit shifting
+     * 
+     * buffer[bufferWriting][counter][0] needs to be set with the control bits
+     * as well as the first few bits of audio
+     * 
+     * buffer[bufferWriting][counter][1] needs to be set with the last audio bits
+     * 
+     * Don't forget to check if you have pause set or not when writing to the buffer
+     * 
+     */
+     
+    // Open the file
+     
+    // Have a loop to read from the file
+     
+    // Close the file
+     
+    //Join and exit the playthread
+
+    return 0;
+}
+
